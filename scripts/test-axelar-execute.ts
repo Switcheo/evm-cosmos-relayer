716525e5cea922b89590b2e43ff6ca5bf6251cfd
Henry Chua <henrychuahb@gmail.com>
2023-11-09 16:17:08 +0800

    add a script to execute stuff on gateway

---
+++ scripts/test-axelar-execute.ts
@@ -0,0 +1,21 @@
+import { evmChains } from '../src/config'
+import { EvmClient } from '../src/clients'
+
+// usage example:
+// ts-node scripts/test-axelar-execute.ts bsc-testnet 0x0000
+async function main() {
+  const chainStr = process.argv[2]
+  const executeData = process.argv[3]
+  console.log('chainStr: ', chainStr)
+  console.log('executeData: ', executeData)
+  const chain = evmChains.filter((v) => v.id === chainStr)[0]
+  const evmClient = new EvmClient(chain)
+
+  const tx = await evmClient.gatewayExecute(executeData);
+  if (!tx) return;
+  console.log(`[handleCosmosToEvmEvent] Execute: ${tx.transactionHash}`);
+}
+
+(async () => {
+  await main()
+})()
