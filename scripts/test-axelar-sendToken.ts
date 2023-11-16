dd8c704c92d002143c2c1d9a72b4131721f2ea1e
Henry Chua <henrychuahb@gmail.com>
2023-11-16 11:17:56 +0800

    Fix bug

--- scripts/test-axelar-sendToken.ts
+++ scripts/test-axelar-sendToken.ts
@@ -2,17 +2,20 @@ import { evmChains } from '../src/config'
 import { EvmClient } from '../src/clients'
 
 // usage example:
-// ts-node scripts/test-axelar-sendToken.ts carbon-localhost tswth1vkutcr2jarnezenf87p4l3grv6h5ra6e30l5gs euaxl 23
+// ts-node scripts/test-axelar-sendToken.ts [from-chain] [to-chain] [to-address] [symbol] [amount]
+// ts-node scripts/test-axelar-sendToken.ts bsc-testnet carbon-localhost tswth1vkutcr2jarnezenf87p4l3grv6h5ra6e30l5gs euaxl 23
 async function main() {
-  const chainStr = process.argv[2]
-  const addr = process.argv[3]
-  const tokenSymbol = process.argv[4]
-  const amount = process.argv[5]
+  const fromChain = process.argv[2]
+  const chainStr = process.argv[3]
+  const addr = process.argv[4]
+  const tokenSymbol = process.argv[5]
+  const amount = process.argv[6]
+  console.log('fromChain: ', fromChain)
   console.log('chainStr: ', chainStr)
   console.log('addr: ', addr)
   console.log('tokenSymbol: ', tokenSymbol)
   console.log('amount: ', amount)
-  const chain = evmChains.filter((v) => v.id === chainStr)[0]
+  const chain = evmChains.filter((v) => v.id === fromChain)[0]
   const evmClient = new EvmClient(chain)
 
   const tx = await evmClient.gateway.sendToken(chainStr, addr, tokenSymbol, amount);
