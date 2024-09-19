41dbb7957c0af2f5273e15b292f383a555e43986
Henry Chua <henrychuahb@gmail.com>
2024-09-19 11:51:30 +0800

    add filter on evm chains to include only config with all fields

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -10,12 +10,17 @@ import devnetEvm from '../../data/devnet/evm.json';
 import { env } from '.';
 import { CosmosNetworkConfig, EvmNetworkConfig } from './types';
 
+// Function to check if all required fields are filled
+const isValidEvmEntry = (entry: any) => {
+  return entry.id && entry.name && entry.finality && entry.rpcUrl && entry.gateway && entry.axelarCarbonGateway;
+};
+
 const cosmos = env.CHAIN_ENV === 'devnet' ? devnetCosmos :
   (env.CHAIN_ENV === 'mainnet' ? mainnetCosmos : testnetCosmos);
 const axelar = env.CHAIN_ENV === 'devnet' ? devnetAxelar :
   (env.CHAIN_ENV === 'mainnet' ? mainnetAxelar : testnetAxelar);
 const evm = env.CHAIN_ENV === 'devnet' ? devnetEvm :
-  (env.CHAIN_ENV === 'mainnet' ? mainnetEvm : testnetEvm);
+  (env.CHAIN_ENV === 'mainnet' ? mainnetEvm : testnetEvm).filter(isValidEvmEntry);
 
 export const cosmosChains: CosmosNetworkConfig[] = cosmos.map((chain) => ({
   ...chain,
