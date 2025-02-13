9e745110f9870b89055751c8f83ad7679461cb56
Henry Chua <henrychuahb@gmail.com>
2025-02-13 13:23:22 +0800

    fix script

--- scripts/fix-hydrogen.ts
+++ scripts/fix-hydrogen.ts
@@ -1,12 +1,14 @@
-import { env } from '../src/config'
+import { env, evmChains } from '../src/config'
 import { fixInTransitFromHydrogen } from '../src/cron'
+import { EvmClient } from '../src'
 
 // usage example:
 // ts-node scripts/fix-stuck-relays.ts
 async function main() {
   console.log('running fix-stuck-relays with hydrogen URL: ', env.HYDROGEN_URL)
 
-  await fixInTransitFromHydrogen(new Date(), new Date())
+  const evmClients = Object.fromEntries(evmChains.map((chain) => [chain.id, new EvmClient(chain)]))
+  await fixInTransitFromHydrogen(evmClients, new Date(), new Date())
 }
 
 (async () => {
