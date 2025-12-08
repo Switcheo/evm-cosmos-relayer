d57e1e3c7f56f8521cd3b1332b2fa5a72fa76453
Henry Chua <henrychuahb@gmail.com>
2025-12-08 14:40:47 +0800

    add sign commands script

---
+++ scripts/test-axelar-sign-commands.ts
@@ -0,0 +1,22 @@
+import { axelarChain } from '../src/config'
+import { AxelarClient, DatabaseClient } from '../src/clients'
+
+// usage example:
+// ts-node scripts/test-sign-commands.ts [chain_id]
+// ts-node scripts/test-sign-commands.ts bsc-testnet
+async function main() {
+  const chain_id = process.argv[2]
+  console.log('chain_id', chain_id)
+
+  const db = new DatabaseClient();
+  const axelarClient = await AxelarClient.init(db, axelarChain);
+  const tx = await axelarClient.signCommands(chain_id)
+
+  if (tx) {
+    console.log(`Confirmed: ${tx.transactionHash}`);
+  }
+}
+
+(async () => {
+  await main()
+})()
